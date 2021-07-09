b78ff9a5af73460f3b3ae17463d4712b86261098
zsarge <zackarywsargent@gmail.com>
2021-07-08 22:16:58 -0400

    add script to download from urls

---
+++ download.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+urls = []
+with open("urls.json") as f:
+    urls = json.load(f)["list"]
+
+if urls == []:
+    exit(1)
+
+def download(url):
+    print(f"downloading {url}...")
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    for br in soup.find_all("br"):
+        br.replace_with("\n")
+    py_results = soup.find_all(class_="python")
+    rb_results = soup.find_all(class_="ruby")
+    return (py_results, rb_results)
+
+
+def save(url):
+    python, ruby = download(url)
+
+    result = ""
+    for r in ruby:
+        result += "====RUBY====="
+        result += r.get_text()
+
+    for p in python:
+        result += "====PYTHON====="
+        result += p.get_text()
+
+    f = open(f"downloads/{url.replace('/', '-')}.txt", "w")
+    f.write(result)
+    f.close()
+
+if __name__ == "__main__":
+    save(urls[0])
